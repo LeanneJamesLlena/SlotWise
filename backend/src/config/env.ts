23fa2d4bd60f14c76da2abdff2a5ce859f968670
Leanne13430 <Leanne.Llena@student.lut.fi>
2026-01-24 19:49:00 +0200

    Added a CI pipeline using github actions to the project

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,6 +3,10 @@ import { z } from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
   CLIENT_URL: z.string().url().optional(),
+  PORT: z.coerce.number().default(4000),
+
+  DATABASE_URL: z.string().min(1),
+
 });
 
 export const env = envSchema.parse(process.env);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,20 @@
-import app from './app.js';
+import 'dotenv/config';
+import { env } from '@/config/env.js';
+import app from '@/app.js';
+import { connectDB } from '@/config/db.js';
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on port ${env.PORT}`);
+    })
+    
+  } catch (error) {
+    console.error('Server startup error:', error);
+    process.exit(1);
+  }
+}
+
+startServer();
