144e55ca378c67c0efa3e8444766467729a83280
Leanne13430 <Leanne.Llena@student.lut.fi>
2026-02-08 16:49:28 +0200

    Format fixed

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { env } from '@/config/env.js';
 
-
-
 export const prisma = new PrismaClient({
   log: env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
