ea5894e76682d01d60b28e8ea006259065fc037c
Leanne13430 <Leanne.Llena@student.lut.fi>
2026-02-11 17:27:39 +0200

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ app.set('trust proxy', 1);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser());
+app.use(cookieParser());d
 
 const corsOptions =
   env.NODE_ENV === 'production'
